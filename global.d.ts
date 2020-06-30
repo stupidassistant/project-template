1090ff5b1243bcf7bb584509201a5b6798265a5a
Thomas Haywood <thomashaywoodproductions@gmail.com>
2020-06-30 16:56:44 +0100

    Fixed TypeScript types

--- global.d.ts
+++ global.d.ts
@@ -1,1 +1,13 @@
-declare function handleRequest(st: string, f: Function): string;
+declare function handleRequest(st: string, f: LamabdaFunction): void;
+
+type LamabdaFunction = (req: LambdaRequest, res: LamdaResponce) => LambdaResult;
+
+type LambdaRequest = {};
+
+type LamdaResponce = {
+  speak(text: string): LamdaResponce
+  text(text: string): LamdaResponce
+  send(): LambdaResult
+};
+
+type LambdaResult = {};
